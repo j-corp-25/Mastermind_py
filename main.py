067b34674c70c8d625768d19b6bf5a23f6d898ba
Jordy <jordycorporan95@gmail.com>
2024-04-15 13:09:20 -0400

    feat: add function to generate sequence

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import requests
+from ascii_logo import logo
 
+URL = "https://www.random.org/integers/?"
+PARAMS = {"num":4,"min":1,"max":6,"col":1,"base":10,"format":"plain","rnd":"new"}
+
+def generate_random_sequence(url,params):
+    response = requests.get(url,params)
+    integer_list = [int(num) for num in response.text.strip().split()]
+    return integer_list
