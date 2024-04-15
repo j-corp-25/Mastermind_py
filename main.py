7c943350c70aeb1649d064eb2621a7f520143166
Jordy <jordycorporan95@gmail.com>
2024-04-15 13:15:11 -0400

    feat(userinput): add function to get players guess
    Added logic inside try blocks for better error handling and better UI if player produces errors

--- main.py
+++ main.py
@@ -8,3 +8,28 @@ def generate_random_sequence(url,params):
     response = requests.get(url,params)
     integer_list = [int(num) for num in response.text.strip().split()]
     return integer_list
+
+def get_players_guess():
+    while True:
+        try:
+            players_guess_input = input("Please guess a combination of four numbers from 0 - 7 (no spaces): ")
+            if len(players_guess_input) == 0:
+                print("Input cannot be empty, You must crack the code!")
+                continue
+            elif len(players_guess_input) > 4:
+                print("That is too many numbers, please input exactly Four numbers")
+                continue
+            elif len(players_guess_input) < 4:
+                print("That is not enough numbers, please input exactly Four numbers")
+                continue
+            try:
+                guess_integers_list = [int(num) for num in players_guess_input.strip()]
+                if len(guess_integers_list) == 4 and all(0 <= num <= 7 for num in guess_integers_list):
+                    return guess_integers_list
+            except ValueError :
+                print("Numbers only please: no extra spaces, or letters or special characters")
+        except KeyboardInterrupt:
+            print("\nQuitting the game.....")
+            break
+        except Exception as error:
+            print(f"An Unexpected Error has Occured {error}")
