aadca9f7ef4128964b30685495653f89161495e0
Jordy <jordycorporan95@gmail.com>
2024-04-17 21:33:47 -0400

    chore: clean up functions and ran formatter

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from rich.layout import Layout
 from rich import box
 from rich.text import Text
 import time
-import sys
+
 console = Console()
 layout = Layout()
 
@@ -34,8 +34,10 @@ styles = [
 ]
 centered_logo = Align.center(logo[3], vertical="middle", style=styles[0])
 
+
 def rich_print_logo():
     rprint(centered_logo)
+
 
 def generate_random_sequence(url, params):
     response = requests.get(url, params)
@@ -60,7 +62,9 @@ def get_players_guess(difficulty_level):
                 continue
             try:
                 guess_integers_list = [int(num) for num in players_guess_input.strip()]
-                if len(guess_integers_list) == 4 and all(0 <= num <= difficulty_level for num in guess_integers_list):
+                if len(guess_integers_list) == 4 and all(
+                    0 <= num <= difficulty_level for num in guess_integers_list
+                ):
                     return guess_integers_list
                 else:
                     print(f"Only Numbers between 0 and {difficulty_level}, Try again!")
@@ -104,20 +108,20 @@ def test_game():
 
 
 def introduce_game():
-    def slow_output(string, speed=0.01):
+    def slow_output(string, speed=0.02):
         for char in string:
-            sys.stdout.write(char)
-            sys.stdout.flush()
+            console.print(char, end="", highlight=True)
             time.sleep(speed)
 
     intro_messages = [
         "Hi Welcome, to Mastermind!\n",
         "# Instructions\n",
         "1. Your task is to guess a 4 number combination\n",
-        "2. You will get Feedback whether your numbers are correct and/or the location is correct\n",
+        "2. You will get feedback on your correct numbers and if the locations are correct\n",
         "3. You have a total of 10 Attempts\n",
         "4. The secret sequence can have duplicate numbers\n"
-        "4. Good luck and have fun\n",
+        "5. The higher the difficulty the higher number range you need to guess *(Easy is from 0 to 7 for a single number)\n"
+        "5. Good luck and have fun\n",
     ]
 
     for message in intro_messages:
@@ -127,24 +131,39 @@ def introduce_game():
 
     if start_game == "y":
         name = input("Awesome, What is your name? ")
-        print(f"{name} is playing")
         return name
     else:
         rprint("Sorry to see you go :loudly_crying_face:")
         return False
 
-def display_game(attempts,board=[],feedbacks=[]):
+
+def display_game(players_name, difficulty, attempts, board, feedbacks):
     game_layout = Layout()
 
-    game_charts = Table(title="Game Session",border_style="red",title_style="bold magenta", header_style="blue",show_header=True, box=box.ROUNDED,expand=True)
+    def convertDifficulty():
+        if difficulty == 7:
+            return "Easy", "Range: 0 to 7"
+        elif difficulty == 8:
+            return "Medium", "Range: 0 to 8"
+        elif difficulty == 9:
+            return "Hard", "Range: 0 to 9"
 
+    level, level_range = convertDifficulty()
 
-    # game_charts.add_column("Attempts",justify="center",ratio=1)
-    game_charts.add_column("Combination Board",justify="center",ratio=1)
-    game_charts.add_column("Feedback",justify="center",ratio=1)
+    game_charts = Table(
+        title="Game Session",
+        border_style="red",
+        title_style="bold magenta",
+        header_style="blue",
+        show_header=True,
+        box=box.ROUNDED,
+        expand=True,
+    )
+    game_charts.add_column("Combination Board", justify="center", ratio=1)
+    game_charts.add_column("Feedback", justify="center", ratio=1, no_wrap=True)
 
     for i in range(len(board)):
-        game_charts.add_row(str(board[i]),(feedbacks[i]))
+        game_charts.add_row(str(board[i]), (feedbacks[i]))
 
     game_layout.split_column(
         Layout(
@@ -155,55 +174,82 @@ def display_game(attempts,board=[],feedbacks=[]):
                 border_style="red",
                 title_align="center",
                 expand=True,
-                padding=(2,2),
-
-
+                padding=(2, 2),
             ),
             name="upper",
             ratio=3,
             minimum_size=15,
-
-
         ),
-        Layout(name="Game Board",ratio=5)
+        Layout(name="Game Board", ratio=5),
     )
 
-    attempts_text = Text(str(attempts), justify='center',style="dark_red")
+    attempts_text = Text(str(attempts), justify="center", style="dark_red")
 
     # Still need to add function
-    difficulty_text = Text("Hard",justify="center",style="orange_red1")
-    hints_text = Text("3/3",justify="center",style="green3")
+    difficulty_text = Text(level, justify="center", style="orange_red1")
+    name_text = Text(players_name, justify="center", style="deep_pink4")
+    hints_text = Text("3/3", justify="center", style="green3")
 
     game_layout["Game Board"].split_row(
-        Layout(name="left",ratio=2),
-        Layout(game_charts, name="right",ratio=6)
+        Layout(name="left", ratio=2), Layout(game_charts, name="right", ratio=6)
     )
     game_layout["left"].split_column(
-        Layout(Panel(attempts_text,style="yellow",title="Attempts Left"),name="left",ratio=3),
-        Layout(Panel(hints_text,style="bright_black",title="Hints Left"),name="center",ratio=3),
-        Layout(Panel(difficulty_text,style="deep_pink2",title="Difficulty Level"),name="lower",ratio=3)
+        Layout(
+            Panel(attempts_text, style="yellow", title="Attempts Left", height=4),
+            name="left",
+            ratio=3,
+        ),
+        Layout(
+            Panel(hints_text, style="bright_black", title="Hints Left", height=4),
+            name="center",
+            ratio=3,
+        ),
+        Layout(
+            Panel(name_text, style="royal_blue1", title="Player", height=4),
+            name="center",
+            ratio=3,
+        ),
+        Layout(
+            Panel(
+                difficulty_text,
+                style="deep_pink2",
+                title="Difficulty Level",
+                height=4,
+                subtitle=level_range,
+            ),
+            name="lower",
+            ratio=3,
+        ),
     )
     rprint(game_layout)
-    
+
+
 def select_difficulty(players_name):
-    difficulty = Prompt.ask("What difficulty do you want?",choices=["easy","medium","hard","insane"],default="Easy")
+    difficulty = Prompt.ask(
+        "What difficulty do you want?",
+        choices=["easy", "medium", "hard"],
+        default="easy",
+    )
     if difficulty == "easy":
-        rprint(f"Wise Choice {players_name}",flush=True)
+        rprint(f"Wise Choice {players_name}")
         time.sleep(1)
         return 7
     elif difficulty == "medium":
-        rprint(f"I wish you the best of luck {players_name}", flush=True)
+        rprint(f"I wish you the best of luck {players_name}")
         time.sleep(1)
         return 8
     elif difficulty == "hard":
-        rprint(f"You're insane {players_name} for choosing this option!!", flush=True)
+        rprint(f"You're insane {players_name} for choosing this option!!")
         time.sleep(1)
         return 9
-    # else:
-    #     rprint(f"This is literally impossible. Good luck {players_name}")
-    #     return 10
 
 
+def get_hint(sequence):
+    return
+
+
+def start_timer():
+    return
 
 
 def play_game():
@@ -221,23 +267,20 @@ def play_game():
     board = []
     feedbacks = []
     while attempts < max_attempts:
-        print(sequence)
         attempts_left = max_attempts - attempts
-        display_game(attempts=attempts_left,board=board,feedbacks=feedbacks)
+        display_game(players_name,max_diff_level,attempts_left,board,feedbacks)
         guess = get_players_guess(max_diff_level)
         board.append(guess)
         feedback = evaluate_players_guess(guess,sequence)
-        attempts += 1
-
         if all(value == 0 for value in feedback.values()):
             feedback_string = "All numbers and locations are incorrect"
         else:
-            feedback_string = f"Correct number(s): {feedback["correct_numbers"]}\t\t\tCorrect location: {feedback['correct_location']}"
-
+            feedback_string = f"Correct number(s): {feedback["correct_numbers"]}\tCorrect location: {feedback['correct_location']}"
         feedbacks.append(feedback_string)
         if feedback["correct_location"] == 4:
             print("Woohooo, you have won")
             break
+        attempts += 1
     else:
         print("Sorry you have failed to crack the code. Better luck next time")
 
